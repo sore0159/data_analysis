2d8457da4934806ee82bfd34032579c2c1c8c3b7
Mule <3.7mules@gmail.com>
2016-12-05 16:34:40 -0600

    added timestamps to maths

--- maths/main.go
+++ maths/main.go
@@ -11,10 +11,12 @@ import (
 )
 
 func main() {
+	log.Println("Starting up...")
 	if len(os.Args) < 2 {
 		log.Println("Usage: maths FILENAME")
 		return
 	}
+	log.Println("Loading CSV File...")
 	list, err := record.FromCSVFile(os.Args[1])
 	if err != nil {
 		log.Println("FILE READ ERROR:", err)
@@ -28,7 +30,9 @@ func main() {
 		log.Println("Not enough values scanned for analysis")
 		return
 	}
+	log.Println("File loaded!")
 
+	log.Println("Parsing data...")
 	d, err := NewData()
 	if err != nil {
 		log.Println("Data loading error: ", err)
@@ -38,9 +42,14 @@ func main() {
 	for _, td := range list {
 		d.AddTweet(td)
 	}
+	log.Println("Data parsed!")
+	log.Println("Running regression...")
 	d.R.Run()
+	log.Println("Regression complete!")
 
 	fmt.Printf("Regression formula (N:%d):\n%v\n", l, d.R.Formula)
+	fmt.Printf("COEF: %v, %v, %v, %v, %v, %v\n", d.R.Coeff(0), d.R.Coeff(1), d.R.Coeff(2), d.R.Coeff(3), d.R.Coeff(4), d.R.Coeff(5))
+	fmt.Printf("R2: %v\n", d.R.R2)
 	//	fmt.Printf("Regression:\n%s\n", d.R)
 }
 
@@ -59,8 +68,8 @@ func (d *Data) TweetDep(td record.TweetData) float64 {
 func (d *Data) TweetInd(td record.TweetData) []float64 {
 	ct, dist := d.C.Closest(td.Location)
 	return []float64{
-		float64(td.TweetCount),
 		float64(td.Links),
+		float64(td.TweetCount),
 		float64(len(td.Words)),
 		float64(dist),
 		float64(ct.Pop),
@@ -70,11 +79,11 @@ func (d *Data) TweetInd(td record.TweetData) []float64 {
 func NewData() (*Data, error) {
 	r := new(regression.Regression)
 	r.SetObserved("Number of Twitter Followers Who Will See This Tweet")
-	r.SetVar(0, "How much the creator has Tweeted before")
-	r.SetVar(1, "How many links are in this Tweet")
+	r.SetVar(0, "How many links are in this Tweet")
+	r.SetVar(1, "How much the creator has Tweeted before")
 	r.SetVar(2, "How many words are in this Tweet")
-	r.SetVar(3, "How close in km is this tweet to a major urban center")
-	r.SetVar(4, "How populous is that major urban center")
+	r.SetVar(3, "How far in km is this tweet to a major urban center (out of top 100)")
+	r.SetVar(4, "How populous is closest major urban center (out of top 100)")
 	c, err := cities.FromFile("../cities/cities.json", 100)
 	if err != nil {
 		return nil, err
